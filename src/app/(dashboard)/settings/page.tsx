e2c51114f7929ea2f1af4c9cd963f7e69c8265d1
Amninder Singh <218442654+amninderprinxe@users.noreply.github.com>
2026-07-23 18:07:47 +0530

    Pass user role to ChangePasswordForm component
    Updated ChangePasswordForm to accept user role as a prop.

--- src/app/(dashboard)/settings/page.tsx
+++ src/app/(dashboard)/settings/page.tsx
@@ -136,7 +136,7 @@ export default async function SettingsPage() {
           </div>
         </div>
         <div className="p-6">
-          <ChangePasswordForm />
+          <ChangePasswordForm role={user.role} />
         </div>
       </div>
 
