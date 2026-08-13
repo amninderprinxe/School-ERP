715aec4f2de3da5fa759780e9b5a67cd3aba718f
amninderprinxe <218442654+amninderprinxe@users.noreply.github.com>
2026-08-13 14:52:07 +0530

    Add Speed Insights

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,24 @@
-import { redirect } from "next/navigation";
+import { Analytics } from "@vercel/analytics/next";
+import { SpeedInsights } from "@vercel/speed-insights/next"
+import type { Metadata } from "next";
+import "./globals.css";
 
-export default function Home() {
-  // Je user login hai taan dashboard te bhej do, nahi taan login page te
-  redirect("/login"); 
+export const metadata: Metadata = {
+  title: "CampusX - School ERP",
+  description: "Automated multi-tenant SaaS School ERP platform",
+};
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="en">
+      <body>
+        {children}
+        <Analytics />
+      </body>
+    </html>
+  );
 }
